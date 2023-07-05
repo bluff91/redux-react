164d7bf838bfe98c07bd8b5e89703622ca371b25
lucian <bluffyx91@gmail.com>
2023-07-05 23:21:00 +0300

    fixed small bug in cartContainer component

--- src/components/cartContainer.jsx
+++ src/components/cartContainer.jsx
@@ -1,15 +1,15 @@
 const CartContainer = () => {
-  //   if (cart.length === 0) {
-  //     return (
-  //       <section className="cart">
-  //         {/* cart header */}
-  //         <header>
-  //           <h2>your bag</h2>
-  //           <h4 className="empty-cart">is currently empty</h4>
-  //         </header>
-  //       </section>
-  //     )
-  //   }
+  if (cart.length === 0) {
+    return (
+      <section className="cart">
+        {/* cart header */}
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    )
+  }
   return (
     <section className="cart">
       {/* cart header */}
